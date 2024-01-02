009f7f76852e4f2e4a9c11ca9d07722bc83b2390
David Kirby <davidkirbymusician@gmail.com>
2024-01-02 16:51:07 +0000

    second commit for deployment - whitenoise.

--- la_selle/settings.py
+++ la_selle/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'la_selle.urls'
@@ -140,5 +142,6 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
