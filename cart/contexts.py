1123f57d671a5949b87a23d96d3ced54eecf6f19
David Kirby <davidkirbymusician@gmail.com>
2024-01-13 13:53:27 +0000

    added cart functionality to cart.contexts.py

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,9 +1,31 @@
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
+
 def cart_contents(request):
 
     cart_items = []
     total = 0
     product_count = 0
     grand_total = 0
+    cart = request.session.get('cart', {}) 
+
+    for item_id, quantity in cart.items(): 
+        product = get_object_or_404(Product, pk=item_id)
+
+        if product.discount:
+            total += quantity * product.discounted_price
+        else:
+            total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    grand_total = total 
+
     context = {
         'cart_items': cart_items,
         'total': total,
