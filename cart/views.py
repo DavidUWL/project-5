a0bbc47cf6cbb23c4c7f0231c54166297814696f
David Kirby <davidkirbymusician@gmail.com>
2024-01-14 21:21:39 +0000

    added cart update quantity functionality, added missing closing div tag

--- cart/views.py
+++ cart/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from decimal import Decimal
 
-# Create your views here.
 
 def view_cart(request):
     return render(request, 'cart/cart.html')
+
 
 def add_to_cart(request, item_id):
 
@@ -17,23 +17,45 @@ def add_to_cart(request, item_id):
 
     cart = request.session.get('cart', {})
 
-    if size: 
+    if size:
         if item_id in list(cart.keys()):
             if size in cart[item_id]['items_by_size'].keys():
                 cart[item_id]['items_by_size'][size] += quantity
-            else: 
+            else:
                 cart[item_id]['items_by_size'] = quantity
         else:
             cart[item_id] = {'items_by_size': {size: quantity}}
-    else: 
+    else:
         if item_id in list(cart.keys()):
             cart[item_id] += quantity
         else:
             cart[item_id] = quantity
 
-
-    
     request.session['cart'] = cart
     print(request.session['cart'])
     return redirect(redirect_url)
 
+
+def adjust_cart(request, item_id):
+
+    quantity = int(request.POST.get('quantity'))
+
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+
+    cart = request.session.get('cart', {})
+
+    if size:
+        if quantity > 0:
+            cart[item_id]['items_by_size'][size] = quantity
+        else:
+            del cart[item_id]['items_by_size'][size]
+    else:
+        if quantity > 0:
+            cart[item_id] = quantity
+        else:
+            cart.pop[item_id]
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
