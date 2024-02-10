2aa4a457c1d90af6cc1f51f13086171f974950e8
David Kirby <davidkirbymusician@gmail.com>
2024-02-10 15:34:44 +0000

    modified email confirmationsubject to correctly call subject.txt file.

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -19,7 +19,7 @@ class StripeWH_Handler:
     def send_order_confirmed_email(self, order):
         customer_email = order.email
         subject = render_to_string(
-            'checkout/emails/order_confirmed_email_body.txt',
+            'checkout/emails/order_confirmed_email_subject.txt',
             {'order': order}
         )
         body = render_to_string(
