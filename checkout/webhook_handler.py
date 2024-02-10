1bac3128ec70c12680ad627c56e59d2006797d27
David Kirby <davidkirbymusician@gmail.com>
2024-02-10 14:07:58 +0000

    modified stripe webhook handler for auto email debugging.

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -20,13 +20,18 @@ class StripeWH_Handler:
         customer_email = order.email
         subject = render_to_string(
             'checkout/emails/order_confirmed_email/order_confirmed_email_subject.txt',
-            {{ 'order': order }}
+            { 'order': order }
         )
         body = render_to_string(
             'checkout/emails/order_confirmed_email/order_confirmed_email_body.txt',
-            {{ 'order': order }}
+            { 'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL }
         )
-        send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [customer_email])
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [customer_email]
+        )
 
     def handle_event(self, event):
         return HttpResponse(
